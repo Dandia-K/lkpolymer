e8c1f209c9841020dad2d3676f6c3ff74ef4cb1b
Dandia-K <dandkim4@gmail.com>
2026-02-20 13:03:57 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,18 +6,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (!menuBtn || !megaMenu || !overlay) return;
 
-  // 햄버거 클릭
   menuBtn.addEventListener("click", function() {
     megaMenu.classList.toggle("active");
     overlay.classList.toggle("active");
-    document.body.classList.toggle("menu-open");
   });
 
-  // 오버레이 클릭
   overlay.addEventListener("click", function() {
     megaMenu.classList.remove("active");
     overlay.classList.remove("active");
-    document.body.classList.remove("menu-open");
   });
 
 });
